a5b066f15d0fc1c4e3acf59d2bf12a8b84a7667f
Sebastian Ernst <semo@Sebastians-MacBook-Pro.local>
2016-05-20 18:15:04 -0600

    Tweaks

---
+++ views/waka.js
@@ -0,0 +1,143 @@
+// here come dat waka boi #1 5:06:28 PM
+// here come dat waka boi #1 5:06:48 PM
+// here come dat waka boi #1 5:10:44 PM 
+// here come dat waka boi #1 5:11:04 PM 
+// here come dat waka boi #1 5:11:23 PM 
+// here come dat waka boi #1 5:12:26 PM
+//  here come dat waka boi #1 5:13:13 PM
+//  here come dat waka boi #1 5:13:33 PM
+// here come dat waka boi #1 5:13:53 PM
+// here come dat waka boi #1 5:14:13 PM
+// here come dat waka boi #1 5:14:33 PM
+// here come dat waka boi #1 5:14:53 PM
+// here come dat waka boi #1 5:15:13 PM
+// here come dat waka boi #1 5:15:33 PM
+// here come dat waka boi #1 5:15:53 PM
+// here come dat waka boi #1 5:16:13 PM
+// here come dat waka boi #1 5:16:33 PM
+// here come dat waka boi #1 5:16:53 PM
+// here come dat waka boi #1 5:17:13 PM
+// here come dat waka boi #1 5:17:33 PM
+// here come dat waka boi #1 5:17:53 PM
+// here come dat waka boi #1 5:18:13 PM
+// here come dat waka boi #1 5:18:33 PM
+// here come dat waka boi #1 5:18:53 PM
+// here come dat waka boi #1 5:19:13 PM
+// here come dat waka boi #1 5:19:33 PM
+// here come dat waka boi #1 5:19:53 PM
+// here come dat waka boi #1 5:20:13 PM
+// here come dat waka boi #1 5:20:33 PM
+// here come dat waka boi #1 5:20:53 PM
+// here come dat waka boi #1 5:21:13 PM
+// here come dat waka boi #1 5:21:33 PM
+// here come dat waka boi #1 5:21:53 PM
+// here come dat waka boi #1 5:22:13 PM
+// here come dat waka boi #1 5:22:33 PM
+// here come dat waka boi #1 5:22:53 PM
+// here come dat waka boi #1 5:23:13 PM
+// here come dat waka boi #1 5:23:33 PM
+// here come dat waka boi #1 5:23:53 PM
+// here come dat waka boi #1 5:24:13 PM
+// here come dat waka boi #1 5:24:33 PM
+// here come dat waka boi #1 5:24:53 PM
+// here come dat waka boi #1 5:25:13 PM
+// here come dat waka boi #1 5:25:33 PM
+// here come dat waka boi #1 5:25:53 PM
+// here come dat waka boi #1 5:26:13 PM
+// here come dat waka boi #1 5:26:33 PM
+// here come dat waka boi #1 5:26:53 PM
+// here come dat waka boi #1 5:27:13 PM
+// here come dat waka boi #1 5:27:33 PM
+// here come dat waka boi #1 5:27:53 PM
+// here come dat waka boi #1 5:28:13 PM
+// here come dat waka boi #1 5:28:33 PM
+// here come dat waka boi #1 5:28:53 PM
+// here come dat waka boi #1 5:29:13 PM
+// here come dat waka boi #1 5:29:33 PM
+// here come dat waka boi #1 5:29:53 PM
+// here come dat waka boi #1 5:30:13 PM
+// here come dat waka boi #1 5:30:33 PM
+// here come dat waka boi #1 5:30:53 PM
+// here come dat waka boi #1 5:31:13 PM
+// here come dat waka boi #1 5:31:33 PM
+// here come dat waka boi #1 5:31:53 PM
+// here come dat waka boi #1 5:32:13 PM
+// here come dat waka boi #1 5:32:33 PM
+// here come dat waka boi #1 5:32:53 PM
+// here come dat waka boi #1 5:33:13 PM
+// here come dat waka boi #1 5:33:33 PM
+// here come dat waka boi #1 5:33:53 PM
+// here come dat waka boi #1 5:34:13 PM
+// here come dat waka boi #1 5:34:33 PM
+// here come dat waka boi #1 5:34:53 PM
+// here come dat waka boi #1 5:35:13 PM
+// here come dat waka boi #1 5:35:33 PM
+// here come dat waka boi #1 5:35:53 PM
+// here come dat waka boi #1 5:36:13 PM
+// here come dat waka boi #1 5:36:33 PM
+// here come dat waka boi #1 5:36:53 PM
+// here come dat waka boi #1 5:37:13 PM
+// here come dat waka boi #1 5:37:33 PM
+// here come dat waka boi #1 5:37:53 PM
+// here come dat waka boi #1 5:38:13 PM
+// here come dat waka boi #1 5:38:33 PM
+// here come dat waka boi #1 5:38:53 PM
+// here come dat waka boi #1 5:39:13 PM
+// here come dat waka boi #1 5:39:33 PM
+// here come dat waka boi #1 5:39:53 PM
+// here come dat waka boi #1 5:40:13 PM
+// here come dat waka boi #1 5:40:33 PM
+// here come dat waka boi #1 5:40:53 PM
+// here come dat waka boi #1 5:41:13 PM
+// here come dat waka boi #1 5:41:33 PM
+// here come dat waka boi #1 5:41:53 PM
+// here come dat waka boi #1 5:42:13 PM
+// here come dat waka boi #1 5:42:33 PM
+// here come dat waka boi #1 5:42:53 PM
+// here come dat waka boi #1 5:43:13 PM
+// here come dat waka boi #1 5:43:33 PM
+// here come dat waka boi #1 5:43:53 PM
+// here come dat waka boi #1 5:44:13 PM
+// here come dat waka boi #1 5:44:33 PM
+// here come dat waka boi #1 5:44:53 PM
+// here come dat waka boi #1 5:45:13 PM
+// here come dat waka boi #1 5:45:33 PM
+// here come dat waka boi #1 5:45:53 PM
+// here come dat waka boi #1 5:46:13 PM
+// here come dat waka boi #1 5:46:33 PM
+// here come dat waka boi #1 5:46:53 PM
+// here come dat waka boi #1 5:47:13 PM
+// here come dat waka boi #1 5:47:33 PM
+// here come dat waka boi #1 5:47:53 PM
+// here come dat waka boi #1 5:48:13 PM
+// here come dat waka boi #1 5:48:33 PM
+// here come dat waka boi #1 5:48:53 PM
+// here come dat waka boi #1 5:49:13 PM
+// here come dat waka boi #1 5:49:33 PM
+// here come dat waka boi #1 5:49:53 PM
+// here come dat waka boi #1 5:50:13 PM
+// here come dat waka boi #1 5:50:33 PM
+// here come dat waka boi #1 5:50:53 PM
+// here come dat waka boi #1 5:51:13 PM
+// here come dat waka boi #1 5:51:33 PM
+// here come dat waka boi #1 5:51:53 PM
+// here come dat waka boi #1 5:52:13 PM
+// here come dat waka boi #1 5:52:33 PM
+// here come dat waka boi #1 5:52:53 PM
+// here come dat waka boi #1 5:53:13 PM
+// here come dat waka boi #1 5:53:33 PM
+// here come dat waka boi #1 5:53:53 PM
+// here come dat waka boi #1 5:54:13 PM
+// here come dat waka boi #1 5:54:33 PM
+// here come dat waka boi #1 5:54:53 PM
+// here come dat waka boi #1 5:55:13 PM
+// here come dat waka boi #1 5:55:33 PM
+// here come dat waka boi #1 5:55:53 PM
+// here come dat waka boi #1 5:56:13 PM
+// here come dat waka boi #1 5:56:33 PM
+// here come dat waka boi #1 5:56:53 PM
+// here come dat waka boi #1 5:57:23 PM
+// here come dat waka boi #1 5:57:43 PM
+// here come dat waka boi #1 5:58:03 PM
+// here come dat waka boi #1 5:59:49 PM
+// 
