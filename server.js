4a37e41fcc61def5c066500a7b72df46ea8c297a
Sebastian Ernst <semo@Sebastians-MacBook-Pro.local>
2016-05-24 00:31:27 -0600

    Changed IP address on server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ import connectMongo from 'connect-mongo';
 const MongoStore = connectMongo(session);
 const Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://hummingbird.email:80/meanmail");
+mongoose.connect("mongodb://159.203.255.205:80/meanmail");
 
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
