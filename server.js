104cc05e66d1d7ff52a4cec439f17480f4210c7a
semoog <sebmernst@gmail.com>
2016-05-24 01:01:11 -0600

    changed port

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ import connectMongo from 'connect-mongo';
 const MongoStore = connectMongo(session);
 const Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://localhost:80/hummingbird");
+mongoose.connect("mongodb://localhost/hummingbird");
 
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
